3dff679b88c629bc2d58e1836a371b88d825a4d1
zyx <1250897150@qq.com>
2020-02-03 17:23:55 +0800

    初始化项目

---
+++ douyu/douyuTests/douyuTests.swift
@@ -0,0 +1,34 @@
+//
+//  douyuTests.swift
+//  douyuTests
+//
+//  Created by 张语喧 on 2020/2/3.
+//  Copyright © 2020 张语喧. All rights reserved.
+//
+
+import XCTest
+@testable import douyu
+
+class douyuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
